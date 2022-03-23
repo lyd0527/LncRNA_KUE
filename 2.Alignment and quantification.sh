6e59cc54634d53324a29639844bf4e48d6aa9195
Yuanda Lv <lyd0527@users.noreply.github.com>
2022-03-23 22:00:54 +0800

    Update 2.Alignment and quantification.sh

--- 2.Alignment and quantification.sh
+++ 2.Alignment and quantification.sh
@@ -9,7 +9,7 @@ nt=30
 STAR --twopassMode Basic --outSAMstrandField intronMotif \
                --outFilterIntronMotifs RemoveNoncanonical --outSAMattrIHstart 0 \
                --outSAMtype BAM SortedByCoordinate --twopass1readsN -1 --outSAMmapqUnique 60 \
-               --genomeDir $db_star --runThreadN $nt --outFileNamePrefix $id \
+               --genomeDir $db_star --runThreadN $nt \
                --readFilesIn $PE1 $PE2
 
 
